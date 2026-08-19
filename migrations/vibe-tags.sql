89772d2d57133f298cdade4d3d47855ad36a36c6
kanika-svg <Kanikalaos003@gmail.com>
2026-08-19 11:41:26 +0700

    Vibe tags on six cafés

---
+++ migrations/vibe-tags.sql
@@ -0,0 +1,6 @@
+UPDATE venues SET vibe = '["under-trees","settle-in"]' WHERE id = 'anthophile';
+UPDATE venues SET vibe = '["under-trees","settle-in"]' WHERE id = 'farsai-cafe';
+UPDATE venues SET vibe = '["tucked-away","for-coffee"]' WHERE id = 'treekoff-watchane';
+UPDATE venues SET vibe = '["tucked-away","settle-in"]' WHERE id = 'drip-1920s';
+UPDATE venues SET vibe = '["for-coffee","settle-in"]' WHERE id = 'sathiti-cafe';
+UPDATE venues SET vibe = '["for-coffee"]' WHERE id = 'maomao-matcha';
